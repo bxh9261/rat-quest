e85bc51501649fcaca5f8e7193a3a5f126631523
gavmat8 <gavmat8@gmail.com>
2020-03-03 11:16:17 -0500

    Healthbars added

--- Rat Quest/Assets/Scripts/Sword.cs
+++ Rat Quest/Assets/Scripts/Sword.cs
@@ -5,22 +5,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sword : MonoBehaviour
 {
     Player m_player;
     float m_damage = 20.0f;
+    float m_enemyMaxHP = 100f;
+    float m_enemyCurrentHP;
+
+    public Slider enemyHealthbar;
 
     // Start is called before the first frame update
     void Start()
     {
         //m_player = GameObject.FindGameObjectWithTag("player").GetComponent<Player>();
+        m_enemyCurrentHP = m_enemyMaxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyHealthbar.value = m_enemyCurrentHP / m_enemyMaxHP;
     }
 
     /// <summary>
@@ -31,6 +37,7 @@ public class Sword : MonoBehaviour
     void DealDamage()
     {
         Debug.Log(m_damage + " damage dealt to enemy.");
+        m_enemyCurrentHP -= m_damage;
     }
 
     /// <summary>
