41cf553036890d324c0523e98cf9c533529eeeea
Gavin <gmd3659@rit.edu>
2020-04-24 22:15:49 -0400

    Main Menu and Game Over created

---
+++ Rat Quest/Assets/Scripts/SceneChanging/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ToMainMenu()
+    {
+        UnitySceneManager.LoadScene("Main Menu");
+    }
+
+    public void ToMainGame()
+    {
+        UnitySceneManager.LoadScene("Main Game");
+    }
+
+    public void ToGameOverScene()
+    {
+        UnitySceneManager.LoadScene("Game Over");
+    }
+}
---
+++ Rat Quest/Assets/Scripts/SceneChanging/SceneFader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image img;
+    public AnimationCurve curve;
+
+    [SerializeField] private float fadeInTime;
+    [SerializeField] private float fadeOutTime;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    /// <summary>
+    /// Fade to a certain scene in game
+    /// </summary>
+    /// <param name="scene">Scene to be transitioned to</param>
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeIn()
+    {
+        Debug.Log("Fade In");
+        float t = fadeInTime;
+
+        while (t > 0f) // Keep animating until t reaches 0
+        {
+            //Debug.Log("t = " + t);
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0; // Skip to the next frame
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeOut(string scene)
+    {
+        float t = 0;
+
+        while (t < fadeOutTime) // Keep animating until t reaches 0
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0; // Skip to the next frame
+        }
+
+        // Load the scene
+        UnitySceneManager.LoadScene(scene);
+    }
+}
